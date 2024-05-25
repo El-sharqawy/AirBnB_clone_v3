adf126c982e3c776ee510f0052b08dcb5e7b366a
El-sharqawy <elsharqawy2@gmail.com>
2024-05-25 15:08:53 +0000

    API

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""app API entry point"""
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+from os import getenv
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """close storage"""
+    storage.close()
+
+
+if __name-_ == "__main__":
+    """setup host and port"""
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+
+    app.run(host=host, port=port, threaded=True)
