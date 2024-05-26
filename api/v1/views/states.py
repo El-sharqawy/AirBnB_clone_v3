8789605e75f0091d4c84536255bbeab4b0dd7f5c
El-sharqawy <elsharqawy2@gmail.com>
2024-05-26 13:01:17 +0000

    API States

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,38 +1,44 @@
 #!/usr/bin/python3
-""" handle all the API actions for states"""
+"""handle all the API actions for states"""
 
 from api.v1.views import app_views
-from flask import jsonify, make_response, request, abort
-from models.state import State
+from flask import abort, jsonify, make_response, request
 from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'])
 def get():
     """get method that retrives states"""
     states = []
+
     for state in storage.all(State).values():
         states.append(state.to_dict())
-    return make_response(jsonify(state), 200)
+
+    return make_response(jsonify(states), 200)
 
 
 @app_views.route('/states/<string:state_id>', methods=['GET'])
 def get_state_id(state_id):
     """get state by ID"""
     state = storage.get(State, state_id)
+
     if state:
         return make_response(jsonify(state.to_dict()), 200)
+
     abort(404)
 
 
 @app_views.route('/states/<string:state_id>', methods=['DELETE'])
 def delete_state_id(state_id):
-    """delete a state using it's ID"""
+    """Delete a state using it's ID"""
     state = storage.get(State, state_id)
+
     if state:
         state.delete()
         storage.save()
         return make_response(jsonify({}), 200)
+
     abort(404)
 
 
@@ -47,26 +53,26 @@ def post_state():
     if 'name' not in state:
         return make_response("Missing name", 400)
 
-    new_state = State(**state)
-    new_state.save()
-    return make_response(jsonify(new_state.to_dict()), 201)
+    newState = State(**state)
+    newState.save()
+    return make_response(jsonify(newState.to_dict()), 201)
 
 
 @app_views.route('/states/<string:state_id>', methods=['PUT'])
 def put_state(state_id):
-    """Update Method to Update states"""
+    """Update State Method"""
     state = storage.get(State, state_id)
-    ignored_keys = ['id', 'updated_at', 'created_at']
-    data = request.get_json()
+    ignored = ['id', 'created_at', 'updated_at']
+    myData = request.get_json()
 
     if not state:
         abort(404)
 
-    if not data:
+    if not myData:
         return make_response("Not a JSON", 400)
 
-    for k, v in data.items():
-        if k not in ignored_keys:
+    for k, v in myData.items():
+        if k not in ignored:
             setattr(state, k, v)
 
     state.save()
