6fa6fd391885b39e257e2c3bceaa792eeb1e31ce
El-sharqawy <elsharqawy2@gmail.com>
2024-05-26 20:45:16 +0000

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""app API entry point"""
+""" app entry point"""
 
 from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
 app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """close storage"""
-    storage.close()
 
 
 @app.errorhandler(404)
 def not_found_404(error):
-    """handle pages that doesn't exist (404)"""
+    """Error 404 Handler"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    """ Close Our Storage"""
+    storage.close()
+
+
 if __name__ == "__main__":
-    """setup host and port"""
+    """Port and Host Initialize"""
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", 5000)
 
